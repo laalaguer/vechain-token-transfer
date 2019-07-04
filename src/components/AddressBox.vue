fe18522858a41952cf18a4a63dfbec67388e65ba
abraham <laalaguer@gmail.com>
2019-07-04 17:29:16 +0800

    minor fix

--- src/components/AddressBox.vue
+++ src/components/AddressBox.vue
@@ -50,6 +50,9 @@ export default {
   },
   computed: {
     computedLabel () {
+      if (this.address === ''){
+        return this.label + ' ' + '...'
+      }
       return this.label + ' ' + this.address.slice(0, 7) + '...' + this.address.slice(-7)
     },
     addressErrorCode () { // Error code of address
